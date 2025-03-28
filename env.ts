6f84e2d38a214a097ff17cd5279ebf044966301c
zft666zft <2810458566@qq.com>
2025-03-28 11:15:43 +0000

    Completed Fan out implementation - added emailing logic

--- env.ts
+++ env.ts
@@ -1,3 +1,3 @@
 export const SES_REGION = 'eu-west-1';
-export const SES_EMAIL_FROM = 'verified-identity-1' ;
-export const SES_EMAIL_TO =  'verified-identity-2';
+export const SES_EMAIL_FROM = '20108799@mail.wit.ie' ;
+export const SES_EMAIL_TO =  'fuz965886@gmail.com';
